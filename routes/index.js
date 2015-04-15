91a4b866c59903d0ef12a8f91b1edc9da2ce855d
Maulik Fojdar <maulik712@gmail.com>
2015-04-15 12:57:18 -0700

    Initial commit
    Sample design for the homepage

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Stock Sentiments' });
+});
+
+/*router.get('/home', function(req, res) {
+  res.render('contact', { title: 'Stock Sentiments' });
+});
+
+router.get('/:company', function(req, res) {
+	  res.render('dashboard', { title: 'Stock Sentiments' });
+	});
+
+router.get('/project', function(req, res) {
+	  res.render('project', { title: 'Stock Sentiments' });
+	});*/
+
+module.exports = router;
