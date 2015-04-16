ab38ba4508ff6d2ed9e65fc02d662a0dfa2a5d33
Maulik Fojdar <maulik712@gmail.com>
2015-04-15 20:51:14 -0700

    Login form - material design

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Stock Sentiments' });
+  res.render('login', { title: 'Stock Sentiments' });
 });
 
 /*router.get('/home', function(req, res) {
